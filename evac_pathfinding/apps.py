91b715bb228fc28775a69d233fad1d2af1b373ca
Afiq Amani <55945064+ahmadafiqamani@users.noreply.github.com>
2025-01-22 09:14:19 +0800

    Delete evac_pathfinding directory

--- evac_pathfinding/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EvacPathfindingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'evac_pathfinding'
--- evac_pathfinding/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'evac_pathfinding'
-
-urlpatterns = [
-    path('path/', views.evacuation_path, name='evacuation_path'),
-]
