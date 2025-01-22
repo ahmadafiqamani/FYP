95c4703fbe645e45f3b16158438364fca1bd04cf
Afiq Amani <55945064+ahmadafiqamani@users.noreply.github.com>
2025-01-22 09:13:45 +0800

    Delete FYP directory

--- FYP/static/js/sidebar_toggle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleBtn = document.getElementById("toggle-btn");
-    const sidebar = document.getElementById("sidebar");
-
-    toggleBtn.addEventListener("click", () => {
-        sidebar.classList.toggle("visible");
-    });
-});
