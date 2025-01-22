a4cbdb84ca2e5ede47483f33db7efff551b8a826
Afiq Amani <55945064+ahmadafiqamani@users.noreply.github.com>
2025-01-22 09:14:04 +0800

    Delete actuator_control directory

--- actuator_control/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ActuatorControlConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'actuator_control'
--- actuator_control/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("state/<str:node>/", views.get_actuator_state, name="get_actuator_state"),
-    path("update/<str:node>/", views.update_actuator_state, name="update_actuator_state"),
-    path("control_panel/", views.control_panel, name="control_panel"),
-]
